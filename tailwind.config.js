1216eb460b07cc039e9a98581872aeed77aae1a1
MineshPC <minesh24patel@gmail.com>
2024-07-29 23:37:18 +0530

    tailwind form plugin 29-07

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx,html}"],
   darkMode: "class",
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms')],
 }
 
